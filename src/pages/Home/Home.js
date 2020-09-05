38dcd139fbaf9f5d1d1de3f638d47568941d6636
christopher124 <63984433+christopher124@users.noreply.github.com>
2020-09-05 17:12:06 -0500

    primer commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,7 @@
 import React from "react";
-import useAuth from "../../hooks/useAuth";
+import "./Home.scss";
 
 export default function Home() {
-  const auth = useAuth;
   return (
     <div>
       <h1>estamos en la home</h1>
